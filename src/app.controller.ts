a0d244d8ece031f873d47c5a163c645f1901a0d2
Viswas kg <viswas.k@provabmail.com>
2020-01-27 19:45:10 +0530

    graphql both front and backend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+import 'cross-fetch/polyfill';
+import gql from 'graphql-tag';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'  
+});
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +16,30 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('accounts')
+  async getAccounts(){
+    try {
+      const result = await client.query({
+        query: gql`
+          query getAccounts {
+            accounts{
+             name
+             branch{
+               name
+             }
+             transactions{
+               amount
+             }
+           }
+           }
+        `,
+      });
+      return result.data;
+    } catch (error) {
+      console.log('error:',error);
+    }
+    return false;
   }
 }
